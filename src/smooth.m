9c4e981bfad3de70c0012e5198d2e4bd5441a53c
yilang zhang <16307130242@fudan.edu.cn>
2019-06-15 10:25:52 +0800

    add dataset

--- src/smooth.m
+++ src/smooth.m
@@ -2,7 +2,7 @@ function [M_smooth] = smooth(M_cummulative)
 %SMOOTH : smooth the motion estimate with Kalman filter or partilce filter.
 %   此处显示详细说明
 % Input: features.
-% Output: cell of smooth matrix.
+% Output: cell of smoothed matrices.
 
 end
 
